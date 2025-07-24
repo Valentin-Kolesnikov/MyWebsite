aea3257ab0bbb34b6ba7e795370955b81bb04186
Valentin Kolesnikov <dima.nedelin2704@gmail.com>
2025-07-25 00:03:03 +0300

    The first upload

---
+++ My HTMLCSSJS web/Javascript/For&Massive.js
@@ -0,0 +1,4 @@
+let trees = ["Яблоки", "Груши", "Апельсины"];
+for (let f = 0; f < trees.length; f++) {
+    console.log("Фрукты:" + trees[f])
+}
---
+++ My HTMLCSSJS web/Javascript/Functions.js
@@ -0,0 +1,8 @@
+let ages = 19;
+if (ages < 18) { // if=если
+    console.log("Ты малютка");
+} else if (ages >= 18 && ages < 65) { // else if=если иначе
+    console.log("Ты взрослый");
+} else { // else=во всех остальных случаях
+    console.log("Ты старик");
+}
